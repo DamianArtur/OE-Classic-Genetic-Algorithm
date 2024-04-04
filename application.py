94c9350801988d7cddf7fafd344b319ddefaf488
DamianArtur <damian.mizera@outlook.com>
2024-04-04 17:03:50 +0200

    Add GUI draft

---
+++ application.py
@@ -0,0 +1,38 @@
+import tkinter as tk
+from tkinter import ttk
+
+class Application(tk.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.master = master
+        self.pack(padx=10, pady=10)
+        self.create_widgets()
+
+    def create_widgets(self):
+        self.function_combo = ttk.Combobox(self, width=30)
+        self.function_combo.pack(pady=5)
+
+        self.input_fields = ["begin of the range", "end of the range", "population amount", "number of bits", 
+                             "best and tournament chromosome amount", "elite strategy amount", 
+                             "cross probability", "mutation probability", "inversion probability"]
+        self.entries = {}
+        for field in self.input_fields:
+            self.entries[field] = tk.Entry(self)
+            self.entries[field].insert(0, field)
+            self.entries[field].bind("<FocusIn>", self.clear_placeholder)
+            self.entries[field].pack(pady=5)
+
+        self.selection_combo = ttk.Combobox(self, width=30)
+        self.selection_combo.pack(pady=5)
+
+        self.cross_combo = ttk.Combobox(self, width=30)
+        self.cross_combo.pack(pady=5)
+
+        self.mutation_combo = ttk.Combobox(self, width=30)
+        self.mutation_combo.pack(pady=5)
+
+        self.max_min_combo = ttk.Combobox(self, width=30)
+        self.max_min_combo.pack(pady=5)
+
+    def clear_placeholder(self, event):
+        event.widget.delete(0, "end")
