e396c7946d84e644d05c1fc46846d00520ca74f7
YuriiMaisuradze <maisooradze@gmail.com>
2024-04-05 02:01:12 +0200

    Connected inversion function to GUI for testing.

--- application.py
+++ application.py
@@ -1,6 +1,10 @@
 import tkinter as tk
 from tkinter import ttk
 
+from BinaryChromosome import GeneticAlgorithm
+from Inversion import InversionOperator
+
+
 class Application(tk.Frame):
     def __init__(self, master=None):
         super().__init__(master)
@@ -33,6 +37,31 @@ class Application(tk.Frame):
 
         self.max_min_combo = ttk.Combobox(self, width=30)
         self.max_min_combo.pack(pady=5)
+        self.apply_button = tk.Button(self, text="Apply Inversion Probability",
+                                      command=self.apply_inversion_probability)
+        self.apply_button.pack(pady=5)
+
+    def apply_inversion_probability(self):
+        inversion_probability = float(
+            self.entries["inversion probability"].get())
+        a = int(
+            self.entries["begin of the range"].get())
+        b = int(
+            self.entries["end of the range"].get())
+        precision = int(
+            self.entries["number of bits"].get())
+        population_size = int(
+            self.entries["population amount"].get())
+        geneticAlgorithm = GeneticAlgorithm(population_size,a,b,precision,100)
+        inversion_operator = InversionOperator(inversion_probability)
+        print("Do inwersji:")
+        for i in range (0,4):
+            print(geneticAlgorithm.population[i].bits)
+        for i in range (0,population_size):
+            inversion_operator.apply(geneticAlgorithm.population[i].bits)
+        print("Po inwersji:")
+        for i in range (0,4):
+            print(geneticAlgorithm.population[i].bits)
 
     def clear_placeholder(self, event):
         event.widget.delete(0, "end")
